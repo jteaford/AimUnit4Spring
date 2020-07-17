65a523e09e9202036e1e71645cdcc6183eca8285
jteaford <jessteaford@gmail.com>
2020-07-16 19:25:31 -0500

    Initial Commit

--- movie/src/main/java/org/aim/movie/Actor.java
+++ movie/src/main/java/org/aim/movie/Actor.java
@@ -1,6 +1,9 @@
 package org.aim.movie;
 
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "actors")
@@ -15,5 +18,36 @@ public class Actor {
 
     private Date dateOfBirth;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
     
 }
---
+++ movie/src/main/java/org/aim/movie/ActorController.java
@@ -0,0 +1,20 @@
+package org.aim.movie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(path = "/api/actors")
+
+public class ActorController {
+    
+    @Autowired
+    private ActorRepository actorRepository;
+
+    public @ResponseBody Iterable<Actor> getAllActors() {
+        return actorRepository.findAll();
+    }
+
+}
