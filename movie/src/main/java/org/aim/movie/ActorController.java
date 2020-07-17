5f4c4651477f86aae308dc1ebc281bf5eb4d937e
jteaford <jessteaford@gmail.com>
2020-07-16 20:24:01 -0500

    Initital Commit

--- movie/src/main/java/org/aim/movie/ActorController.java
+++ movie/src/main/java/org/aim/movie/ActorController.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,7 +23,7 @@ public class ActorController {
         return actorRepository.findAll();
     }
 
-    @PostMapping(path = "")
+    @PostMapping(path = "/")
     public @ResponseBody String createActor(@RequestBody Actor actor) {
         actorRepository.save(actor);
         return "Saved";
