4171d1c81af926dc3057c4a9c6e642ed651a4dfa
jteaford <jessteaford@gmail.com>
2020-07-30 19:20:40 -0500

    Initial Commit

--- movie/frontend-vue/src/views/Actors.vue
+++ movie/frontend-vue/src/views/Actors.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    {{actors}}
   </div>
 </template>
 
 <script>
 export default {
     name: 'Actors',
-    data: () => ({ actors: [] }),
+    data: () => ({ 
+      actors: [] 
+    }),
         async mounted() {
             console.log('actors mounted begin');
             const { data } = await this.$http.get('http://localhost:8080/api/actors');
