c496d7c4b7c11b7256904b86af09a52013dcb936
jteaford <jessteaford@gmail.com>
2020-08-03 12:23:11 -0500

    Initial Commit

--- movie/frontend-vue/src/views/AddMovie.vue
+++ movie/frontend-vue/src/views/AddMovie.vue
@@ -1,25 +1,25 @@
 <template>
     <div>
-        <h1 class="title">Add Actor</h1>
+        <h1 class="title">Add Movie</h1>
 
         <div class="field"> 
-            <label class="label">First Name</label>
+            <label class="label">Movie Title</label>
             <div class="control">
-                <input class="input" type="text" v-model="actor.firstName" placeholder="First Name" />
+                <input class="input" type="text" v-model="actor.movieTitle" placeholder="Movie Title" />
             </div> 
         </div>
 
         <div class="field"> 
-            <label class="label">Last Name</label>
+            <label class="label">Movie Length</label>
             <div class="control">
-                <input class="input" type="text" v-model="actor.lastName" placeholder="Last Name" />
+                <input class="input" type="text" v-model="actor.movieLength" placeholder="Movie Length" />
             </div> 
         </div>
 
         <div class="field"> 
-            <label class="label">Date of Birth</label>
+            <label class="label">Release Date</label>
             <div class="control">
-                <input class="input" type="text" v-model="actor.dateOfBirth" placeholder="Date of Birth" />
+                <input class="input" type="text" v-model="actor.releaseDate" placeholder="Release Date" />
             </div> 
         </div>
    
@@ -36,24 +36,24 @@
 
 <script>
 export default {
-    name: 'AddActor',
+    name: 'AddMovie',
     data: () => ({
-        actor: {
-            firstName: "",
-            lastName: "",
-            dateOfBirth: ""
+        movie: {
+            movieTitle: "",
+            movieLength: "",
+            releaseDate: ""
         }
     }),
 
     methods: {
         cancel() {
-            this.$router.push({path: '/actors'});
+            this.$router.push({path: '/movies'});
         },
         async save() {
-            const response = await this.$http.post('http://localhost:8080/api/actors/', this.actor)
+            const response = await this.$http.post('http://localhost:8080/api/movies/', this.movie)
             console.log(response);
             if (response.status === 200) {
-                this.$router.push({path: '/actors'});
+                this.$router.push({path: '/movies'});
             }
         }
     }
---
+++ movie/frontend-vue/src/views/Movies.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="main">
+    <div class="header">
+      <h1 class="title">Movies</h1>
+    </div>
+
+    <router-link to="/movies" tag="button" class="button is-primary">Add Movie</router-link>
+
+    <div class="content">
+
+      <table id="movie" class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Movie Title</th>
+            <th>Movie Length</th>
+            <th>Release Date</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr v-for="movie in movie" :key="movie.id">
+              <td>{{ movie.id }}</td>
+              <td>{{ movie.movieTitle}}</td>
+              <td>{{ movie.movieLength }}</td>
+              <td>{{ movie.releaseDate }}</td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
